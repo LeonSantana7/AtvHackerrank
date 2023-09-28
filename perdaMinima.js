dbe4204ffaf31a5074f5e6f194ad19f0033c4b44
Leon Santana Barbosa <leon.barbosa@dpe.ba.gov.br>
2023-09-28 11:56:05 -0300

    Fazendo

---
+++ perdaMinima.js
@@ -0,0 +1,14 @@
+//varrer todas as possibilidades de compra e venda
+//ver para cada uma se é prejuizo
+//se for , anotar valor
+
+//dps ver qual foi o menor prejuizo
+
+precos = [20, 15, 8, 2, 12];
+
+for(let i = 0; i < precos.length; i++){
+    for(let j = i + 1; i < precos.length; j++ ){
+        const compra = precos[i];
+        const venda = precos[j];
+    }
+}
