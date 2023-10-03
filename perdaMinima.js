1f04ee7c396bd5818f7c97e3815c9c9c35e3100c
Leon Santana <leon.santana854@gmail.com>
2023-10-03 18:36:48 -0300

    Atividades Feitas

--- perdaMinima.js
+++ perdaMinima.js
@@ -3,12 +3,21 @@
 //se for , anotar valor
 
 //dps ver qual foi o menor prejuizo
-
+let menorPrejuizo = 999999999999;
 precos = [20, 15, 8, 2, 12];
 
 for(let i = 0; i < precos.length; i++){
-    for(let j = i + 1; i < precos.length; j++ ){
+    for(let j = i + 1; j < precos.length; j++ ){
         const compra = precos[i];
         const venda = precos[j];
+        const prejuizo = compra - venda;
+
+        if(prejuizo > 0){
+            //realmente é prejuizo
+            if(prejuizo < menorPrejuizo){
+                menorPrejuizo = prejuizo;
+            }
+        }
     }
+    console.log(menorPrejuizo);
 }
